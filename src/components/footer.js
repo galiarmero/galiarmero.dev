da54c2c6da8efdf85f609e870d0f6b8d7e9fcb96
galiarmero <galiarmero.dev@gmail.com>
2020-02-25 18:40:44 +0800

    Style footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,19 @@
 import React from "react"
+import { css } from "@emotion/core"
 
 export default () => (
-  <footer>
-    <h6><a href="https://github.com/galiarmero/.dev">Built with &lt;3 and Gatsby</a></h6>
-    <h6>Crafted by Gali Armero &copy; 2020</h6>
+  <footer css={css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: var(--boxShadowColor);
+    height: auto;
+    min-height: 80px;
+    padding: 15px 0;
+  `}>
+    <h6>Built with <a href="https://www.gatsbyjs.org/">Gatsby</a> · Hosted on <a href="https://www.netlify.com/">Netlify</a></h6>
+    <h6><a href="https://github.com/galiarmero/.dev">Crafted with &lt;3 by Gali Armero</a></h6>
+    <h6>&copy; 2020</h6>
   </footer>
 )
