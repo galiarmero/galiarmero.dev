137b8a08c961e785e1d1487ca68e270674d8777b
galiarmero <galiarmero.dev@gmail.com>
2020-03-06 20:32:47 +0800

    Put some twist in footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,11 +7,12 @@ export default () => (
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background: var(--boxShadowColor);
+    background: var(--textColor);
     height: auto;
     min-height: 90px;
     padding: 15px 0;
-    font-size: .7rem;
+    font-size: .8rem;
+    color: var(--bgColor);
   `}>
     <span>Built with <a href="https://www.gatsbyjs.org/">Gatsby</a> · Hosted on <a href="https://www.netlify.com/">Netlify</a></span>
     <span><a href="https://github.com/galiarmero/.dev">Crafted with &lt;3 by Gali Armero</a></span>
