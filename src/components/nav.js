a1b5d18f27b6f001d43884bf5eb26aaa4aab53de
galiarmero <galiarmero.dev@gmail.com>
2020-03-15 23:24:40 +0800

    Burger design

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { css } from "@emotion/core"
+import styled from "@emotion/styled"
+import { colors } from '../styles/theme'
 
 export default (props) => (
   <header css={css`
@@ -13,11 +15,6 @@ export default (props) => (
     -webkit-box-shadow: 0px 0.1rem 0.2rem 0px var(--boxShadowColor);
     -moz-box-shadow: 0px 0.1rem 0.2rem 0px var(--boxShadowColor);
   `}>
-    <nav css={css`
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-    `}>
       <div css={css`
         display: flex;
         justify-content: space-between;
@@ -25,9 +22,44 @@ export default (props) => (
         height: 75px;
       `}>
         <div className="logo"><h1>G</h1></div>
-        <div><button id="burger" onClick={props.onBurgerClick}>=</button></div>
+        <div onClick={props.onBurgerClick} css={css`
+          display: -webkit-box;
+          display: flex;
+          margin-left: auto;
+          width: 2rem;
+          height: 1.3rem;
+          -webkit-box-orient: vertical;
+          -webkit-box-direction: normal;
+                  flex-direction: column;
+          -webkit-box-pack: justify;
+                  justify-content: space-between;
+          -webkit-box-align: center;
+                  align-items: center;
+          cursor: pointer;
+        `}>
+          <BurgerBar css={css`opacity: ${props.isMenuVisible ? `0` : `1`}`}></BurgerBar>
+          <BurgerBar css={css`
+            ${props.isMenuVisible ? `
+              -webkit-transform: rotate(45deg);
+              transform: rotate(45deg);
+              position: relative;
+              &:after {
+                content: "";
+                background: ${colors.accent};
+                width: 100%;
+                height: 1.2px;
+                position: absolute;
+                display: inline-block;
+                -webkit-transform: rotate(-90deg);
+                        transform: rotate(-90deg);
+              }
+            ` : ``}
+          `}></BurgerBar>
+          <BurgerBar css={css`opacity: ${props.isMenuVisible ? `0` : `1`}`}></BurgerBar>
+        </div>
       </div>
-      <div css={css`
+
+      {/* <nav css={css`
         display: ${props.isMenuVisible ? `flex` : `none`};
         flex-direction: column;
         justify-content: center;
@@ -37,7 +69,15 @@ export default (props) => (
         <div className="nav-item">About</div>
         <div className="nav-item">Work</div>
         <div className="nav-item">Blog</div>
-      </div>
-    </nav>
+      </nav> */}
   </header>
 )
+
+const BurgerBar = styled.span`
+  background-color: ${colors.accent};
+  display: inline-block;
+  height: 1.2px;
+  width: 100%;
+  -webkit-transition: all 90ms ease;
+  transition: all 90ms ease;
+`
