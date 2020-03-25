78351a2b2de65361245481e01ef159d18a08258d
galiarmero <galiarmero.dev@gmail.com>
2020-03-25 18:12:34 +0800

    Stylize stand burger

--- src/components/stand-burger.js
+++ src/components/stand-burger.js
@@ -24,44 +24,40 @@ export default ({ onToggleMenu, isMenuOpen }) => (
     <span className="hamburger-box" css={css`
       position: relative;
       display: inline-block;
-      width: 40px;
-      height: 24px;
+      width: 32px;
+      height: 22px;
     `}>
       <span className="hamburger-inner" css={css`
         top: 50%;
         display: block;
         margin-top: -2px;
-
         position: absolute;
-        width: 40px;
-        height: 2.5px;
+        width: 32px;
+        height: 2px;
         transition-timing-function: ease;
         transition-duration: .15s;
         transition-property: transform;
         border-radius: 4px;
-
         transition: ${isMenuOpen
                     ? `transform 75ms cubic-bezier(.215,.61,.355,1) 0s,background-color 0s linear .15s`
                     : `transform 75ms cubic-bezier(.55,.055,.675,.19) .15s,background-color 0s linear 75ms`};
         transform: ${isMenuOpen
                     ? `rotate(90deg)`
                     : `none`};
-        background-color: ${isMenuOpen ? `transparent` : `#fff`};
+        background-color: ${isMenuOpen ? `transparent` : `var(--accentColor)`};
 
         &:before {
           position: absolute;
-          width: 40px;
-          height: 2.5px;
+          width: ${isMenuOpen ? `32px` : `20px`};
+          height: 2px;
           transition-timing-function: ease;
           transition-duration: .15s;
           transition-property: transform;
           border-radius: 4px;
-          background-color: #fff;
-
+          background-color: var(--accentColor);
           display: block;
           content: "";
-
-          top: ${isMenuOpen ? `0` : `-10px`};
+          top: ${isMenuOpen ? `0` : `-9px`};
           transition: ${isMenuOpen
                       ? `top 75ms ease-out .1s,transform 75ms cubic-bezier(.215,.61,.355,1) .15s`
                       : `top 75ms ease-in 75ms,transform 75ms cubic-bezier(.55,.055,.675,.19) 0s`};
@@ -72,18 +68,18 @@ export default ({ onToggleMenu, isMenuOpen }) => (
 
         &:after {
           position: absolute;
-          width: 40px;
-          height: 2.5px;
+          width: ${isMenuOpen ? `32px` : `20px`};
+          height: 2px;
           transition-timing-function: ease;
           transition-duration: .15s;
           transition-property: transform;
           border-radius: 4px;
-          background-color: #fff;
-
+          background-color: var(--accentColor);
           display: block;
           content: "";
+          right: ${isMenuOpen ? `none` : `0`};
 
-          bottom: ${isMenuOpen ? `0` : `-10px`};
+          bottom: ${isMenuOpen ? `0` : `-9px`};
           transition: ${isMenuOpen
                       ? `bottom 75ms ease-out .1s,transform 75ms cubic-bezier(.215,.61,.355,1) .15s`
                       : `bottom 75ms ease-in 75ms,transform 75ms cubic-bezier(.55,.055,.675,.19) 0s`};
