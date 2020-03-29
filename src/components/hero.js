b2feeefa5579ddd176282a18a7b0f82b0a117bb1
galiarmero <galiarmero.dev@gmail.com>
2020-03-29 22:02:37 +0800

    Adjust visibility sensor on hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,13 +4,13 @@ import VizSensor from "react-visibility-sensor"
 
 export default ({ greeting, name, tagline, onChangeVisiblity }) => {
   return (
-    <VizSensor onChange={onChangeVisiblity}>
-      <section css={css`
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-      `}>
+    <section css={css`
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    `}>
+      <VizSensor onChange={onChangeVisiblity}>
         <span css={css`
           padding: 0 0 10px 3px;
           color: var(--accentColor);
@@ -33,40 +33,40 @@ export default ({ greeting, name, tagline, onChangeVisiblity }) => {
         `}>
           {tagline}
         </h1>
+      </VizSensor>
 
+      <div css={css`
+        width: 30px;
+        position: absolute;
+        bottom: 0;
+        left: auto;
+        right: 15px;
+        z-index: 200;
+        color: var(--textColor);
+      `}>
         <div css={css`
-          width: 30px;
-          position: absolute;
-          bottom: 0;
-          left: auto;
-          right: 15px;
-          z-index: 200;
-          color: var(--textColor);
-        `}>
-          <div css={css`
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            position: relative;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          position: relative;
 
-            &:after {
-              content: "";
-              display: block;
-              width: 2px;
-              height: 45px;
-              margin: 0px auto;
-              background-color: var(--textColor);
-            }
-          `}>
-            <span css={css`
-              writing-mode: vertical-rl;
-              font-size: 0.65rem;
-              letter-spacing: 0.3rem;
-              font-family: 'JetBrainsMono-Regular';
-            `}>SCROLL</span>
-          </div>
+          &:after {
+            content: "";
+            display: block;
+            width: 2px;
+            height: 45px;
+            margin: 0px auto;
+            background-color: var(--textColor);
+          }
+        `}>
+          <span css={css`
+            writing-mode: vertical-rl;
+            font-size: 0.65rem;
+            letter-spacing: 0.3rem;
+            font-family: 'JetBrainsMono-Regular';
+          `}>SCROLL</span>
         </div>
-      </section>
-    </VizSensor>
+      </div>
+    </section>
   )
 }
