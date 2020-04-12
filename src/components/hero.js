4174eaabccf76d95450ec5ad1f8ed353b75f1fb9
galiarmero <galiarmero.dev@gmail.com>
2020-04-12 23:13:11 +0800

    Transitions on hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,53 +1,82 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { css } from "@emotion/core"
 import IntersectionObserver from "@researchgate/react-intersection-observer"
+import { TransitionGroup, CSSTransition } from "react-transition-group"
 
 import settings from "../config/settings"
 
 
-export default ({ handleIntersection, greeting, name, tagline }) => {
+export default (props) => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), 1500);
+    return () => clearTimeout(timeout);
+  }, []);
+
   const onChange = ({ time, isIntersecting, intersectionRatio }) => {
-    handleIntersection({
+    props.handleIntersection({
       hero: { time, isIntersecting, intersectionRatio },
     })
   }
   const options = {
     onChange: onChange,
     threshold: settings.intersectionObserverThreshold,
-  };
+  }
+
+  const greeting = (
+    <div css={css`transition-delay: 100ms`}>
+      <span css={css`
+        padding: 0 0 10px 3px;
+        color: var(--accentColor);
+        font-family: 'JetBrainsMono-Regular';
+        font-size: 0.9rem;
+      `}>
+        {props.greeting}
+      </span>
+    </div>
+  )
+
+  const name = (
+    <div css={css`transition-delay: 300ms`}>
+      <h1 css={css`
+        font-size: 2.5rem;
+      `}>
+        {props.name}.
+      </h1>
+    </div>
+  )
+
+  const tagline = (
+    <div css={css`transition-delay: 500ms`}>
+      <h1 css={css`
+        font-size: 2.5rem;
+        font-family: 'Gilroy-Light', sans-serif;
+        font-weight: 300;
+      `}>
+        {props.tagline}
+      </h1>
+    </div>
+  )
+
+  const items = [greeting, name, tagline]
 
   return (
     <IntersectionObserver {...options}>
-      <section as="section" css={css`
+      <TransitionGroup component="section" css={css`
         min-height: 140vh;
         display: flex;
         flex-direction: column;
         justify-content: start;
         padding-top: 30vh;
       `}>
-        <span css={css`
-          padding: 0 0 10px 3px;
-          color: var(--accentColor);
-          font-family: 'JetBrainsMono-Regular';
-          font-size: 0.9rem;
-        `}>
-          {greeting}
-        </span>
-
-        <h1 css={css`
-          font-size: 2.5rem;
-        `}>
-          {name}.
-        </h1>
-
-        <h1 css={css`
-          font-size: 2.5rem;
-          font-family: 'Gilroy-Light', sans-serif;
-          font-weight: 300;
-        `}>
-          {tagline}
-        </h1>
-
+        { isMounted &&
+          items.map((item, i) => (
+            <CSSTransition key={i} classNames="fadeup" timeout={3000}>
+              {item}
+            </CSSTransition>
+          ))
+        }
         <div css={css`
           width: 30px;
           position: absolute;
@@ -80,7 +109,7 @@ export default ({ handleIntersection, greeting, name, tagline }) => {
             `}>SCROLL</span>
           </div>
         </div>
-      </section>
+      </TransitionGroup>
     </IntersectionObserver>
   )
 }
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -119,5 +119,17 @@ export default props => (
       margin-bottom: 1.6rem;
       overflow-wrap: break-word;
     }
+
+    .fadeup-enter {
+      opacity: 0.01;
+      transform: translateY(20px);
+      transition: opacity 300ms cubic-bezier(0.645, 0.045, 0.355, 1), transform 400ms cubic-bezier(0.645, 0.045, 0.355, 1);
+    }
+
+    .fadeup-enter-active {
+      opacity: 1;
+      transform: translateY(0px);
+      transition: opacity 300ms cubic-bezier(0.645, 0.045, 0.355, 1), transform 400ms cubic-bezier(0.645, 0.045, 0.355, 1);
+    }
   `} />
 )
