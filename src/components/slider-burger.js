977a508f1ac17eebf2a4ee12a9d876bbf9af5087
galiarmero <galiarmero.dev@gmail.com>
2020-04-29 20:44:32 +0800

    Configurable slider hamburger

--- src/components/slider-burger.js
+++ src/components/slider-burger.js
@@ -1,9 +1,18 @@
 import React from "react"
 import { css } from "@emotion/core"
 
+const hamburgerPaddingY = `5px`
+const hamburgerPaddingX = `5px`
+const hamburgerLayerWidth = `32px`
+const hamburgerLayerHeight = `2px`
+const hamburgerLayerSpacing = `6px`
+const hamburgerLayerColor = `var(--headingColor)`
+const hamburgerLayerBorderRadius = `4px`
+const yOffset = `calc(${hamburgerLayerSpacing} + ${hamburgerLayerHeight})`
+
 export default ({ onToggleMenu, isMenuOpen }) => (
   <button className="hamburger" type="button" onClick={onToggleMenu} css={css`
-    padding: 6px 6px;
+    padding: ${hamburgerPaddingY} ${hamburgerPaddingX};
     display: inline-block;
     cursor: pointer;
     transition-property: opacity, filter;
@@ -25,79 +34,65 @@ export default ({ onToggleMenu, isMenuOpen }) => (
     }
   `}>
     <span className="hamburger-box" css={css`
-      width: 32px;
-      height: 20px;
+      width: ${hamburgerLayerWidth};
+      height: calc(${hamburgerLayerHeight} * 3 + ${hamburgerLayerSpacing} * 2);
       display: inline-block;
       position: relative;
-      margin-top: 7px;
+      margin-top: ${hamburgerLayerSpacing};
     `}>
       <span className="hamburger-inner" css={css`
         display: block;
         top: 50%;
-        margin-top: -1px;
+        margin-top: calc(${hamburgerLayerHeight} / -2);
 
-        width: 32px;
-        height: 2px;
-        background-color: #fff;
-        border-radius: 4px;
-        position: absolute;
-        transition-property: transform;
-        transition-duration: 0.15s;
-        transition-timing-function: ease;
-
-        top: 1px;
-
+        // Slider-r
+        top: calc(${hamburgerLayerHeight} / 2);
         transform: ${isMenuOpen
-                      ? `translate3d(0, 9px, 0) rotate(45deg)`
+                      ? `translate3d(0, ${yOffset}, 0) rotate(-45deg)`
                       : `none`};
 
-        &:before {
-          width: 32px;
-          height: 2px;
-          background-color: #fff;
-          border-radius: 4px;
+        &,
+        &::before,
+        &::after {
+          width: ${hamburgerLayerWidth};
+          height: ${hamburgerLayerHeight};
+          background-color: ${hamburgerLayerColor};
+          border-radius: ${hamburgerLayerBorderRadius};
           position: absolute;
           transition-property: transform;
           transition-duration: 0.15s;
           transition-timing-function: ease;
+        }
 
+        &::before,
+        &::after {
           content: "";
           display: block;
+        }
 
-          top: -9px;
+        &:before {
+          top: calc((${hamburgerLayerSpacing} + ${hamburgerLayerHeight}) * -1);
 
-          top: 9px;
+          // Slider-r
+          top: calc(${hamburgerLayerHeight} + ${hamburgerLayerSpacing});
           transition-property: transform, opacity;
           transition-timing-function: ease;
           transition-duration: 0.15s;
-
           transform: ${isMenuOpen
-            ? `rotate(-45deg) translate3d(-4.57143px, -7px, 0)`
-            : `none`};
+                        ? `rotate(-45deg) translate3d(calc(${hamburgerLayerWidth} / 7), calc(${hamburgerLayerSpacing} * -1), 0)`
+                        : `none`};
           opacity: ${isMenuOpen
                       ? `0`
                       : `1`};
         }
 
         &:after {
-          width: 32px;
-          height: 2px;
-          background-color: #fff;
-          border-radius: 4px;
-          position: absolute;
-          transition-property: transform;
-          transition-duration: 0.15s;
-          transition-timing-function: ease;
+          bottom: calc((${hamburgerLayerSpacing} + ${hamburgerLayerHeight}) * -1);
 
-          content: "";
-          display: block;
-
-          bottom: -9px;
-
-          top: 18px;
-
+          // Slider-r
+          top: calc((${hamburgerLayerHeight} * 2) + (${hamburgerLayerSpacing} * 2));
           transform: ${isMenuOpen
-            ? `translate3d(0, -18px, 0) rotate(-90deg)`
+            ? `translate3d(0, calc(${yOffset} * -2), 0) rotate(-90deg)`
             : `none`};
         }
       `}>
