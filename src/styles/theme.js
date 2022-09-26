5465ba49f57fa31705220930de516ea5bfd8a9e7
Gali Armero <galiarmero.dev@gmail.com>
2022-09-26 20:51:38 +0800

    Some styling updates on blog posts

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,10 +1,12 @@
 export const colors = {
   bg: "#011321",
+  lightestBg: "#13334B",
   lighterBg: "#071D2D",
   darkerBg: "#010F1A",
   selectBg: "#1428C4",
   subtleLine: "#122433",
   heading: "#FFF",
+  softText: "#FFF",
   text: "#FFFFFFBF",
   accent: "#00DFFC",
   boxShadow: "rgba(1, 12, 20, 0.7)",
