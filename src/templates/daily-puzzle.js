a11c148b2213e037b98b972d21ea454b42fed05e
Gali Armero <galiarmero.dev@gmail.com>
2022-10-17 22:44:16 +0800

    Masonry layout in daily-puzzles page (#68)
    * Masonry layout for puzzle scores
    
    * Format

--- src/templates/daily-puzzle.js
+++ src/templates/daily-puzzle.js
@@ -4,25 +4,43 @@ import { css } from "@emotion/react"
 import Linkify from "linkify-react"
 import "linkify-plugin-mention"
 import "linkify-plugin-hashtag"
+import styled from "@emotion/styled"
 import Twemoji from "react-twemoji"
 
 import Helmet from "../components/helmet"
 import Header from "../components/header"
 import Footer from "../components/footer"
 // import Engage from "../components/engage"
-import { Main, AutoFitGrid } from "../styles/Containers"
+import { Main, Masonry, Card } from "../styles/Containers"
 import GlobalStyles from "../styles/GlobalStyles"
 import DailyPuzzleStyles from "../styles/DailyPuzzleStyles"
 import { SectionHeading } from "../styles/Headings"
 import { colors, breakpoint } from "../styles/theme"
 import siteData from "../config/site-data.yml"
-import { formatDateWithDayName, newlineToBr } from "../utils"
+import { formatDateWithDayName } from "../utils"
 
 const { profile, siteBaseUrl } = siteData
 const HEADER_HEIGHT = 75
+const PUZZLE_ORDER = [
+  "wordle",
+  "quordle",
+  "worldle",
+  "saltong",
+  "saltong-mini",
+  "waffle",
+]
+
+const PuzzleBox = styled(Card)`
+  justify-content: flex-start;
+  grid-row: span ${(props) => props.rowSpan};
+`
 
 const DailyPuzzle = ({ data, pageContext }) => {
-  const puzzles = data.allPuzzleScores.edges
+  const puzzles = data.allPuzzleScores.edges.sort((a, b) => {
+    return (
+      PUZZLE_ORDER.indexOf(a.node.puzzle) - PUZZLE_ORDER.indexOf(b.node.puzzle)
+    )
+  })
   const { slug, date } = pageContext
   const displayDate = formatDateWithDayName(date)
 
@@ -54,19 +72,19 @@ const DailyPuzzle = ({ data, pageContext }) => {
           </SectionHeading>
         </div>
 
-        <AutoFitGrid marginTop={`50px`}>
+        <Masonry marginTop={`50px`}>
           {puzzles.length > 0 &&
             puzzles.map(({ node }, i) => (
-              <div key={node.id}>
+              <PuzzleBox key={node.id} rowSpan={calculateSpan(node.resultText)}>
                 <h3>
                   {node.puzzle} {node.dayNumber}
                 </h3>
                 <div class="puzzle-score">
                   {renderPuzzleResult(node.puzzle, node.resultText)}
                 </div>
-              </div>
+              </PuzzleBox>
             ))}
-        </AutoFitGrid>
+        </Masonry>
       </Main>
       <Footer />
     </div>
@@ -103,6 +121,10 @@ const overridePuzzleResult = (puzzle, text) => {
   }
 
   return text
+}
+
+const calculateSpan = (text) => {
+  return text.split("\n").length + 4
 }
 
 export default DailyPuzzle
